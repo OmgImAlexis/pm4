662b081e95194309f233d67063e598449721a507
Alexis <xo@wvvw.me>
2021-09-26 10:20:25 +0930

    feat: automatically use local config.json and add config loading error

--- src/god/commands/start.ts
+++ src/god/commands/start.ts
@@ -1,13 +1,31 @@
-import { readFileSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 import { Command } from "../../common/types";
 import { App, apps, Config } from '../apps';
 import { startApp, getAppStatus } from '../common';
 
+const getConfig = (configPath: string) => {
+    try {
+        return JSON.parse(readFileSync(configPath, 'utf8')) as Config;
+    } catch {
+        return undefined;
+    }
+};
+
 export const startCommand: Command = {
     name: 'start',
     async method([appName, ..._args], flags) {
-        // The user provided config
-        const config = flags.config ? JSON.parse(readFileSync(flags.config, 'utf8')) as Config : undefined;
+        if (!appName) {
+            throw new Error('No app name provided.');
+        }
+
+        // Ensure we only pass a string through
+        const configPath = typeof flags.config === 'string' ? flags.config : undefined;
+
+        // Check if the user provided config exists
+        if (configPath && !existsSync(configPath)) throw new Error(`Config path ${configPath} doesn't exist.`);
+
+        // Get the user's config
+        const config = getConfig(configPath ?? './config.json');
 
         // Get the app
         const app: Partial<App> | undefined = apps.get(appName) ?? config?.apps?.find(app => app.name === appName);
